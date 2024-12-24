59810fbfb1226cb46154f7025f68787376affba3
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-12-23 20:41:28 -0500

    initial commit

---
+++ include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Kailin on 12/23/24.
+//
+
+#ifndef OPERATORS_MAIN_H
+#define OPERATORS_MAIN_H
+
+#endif //OPERATORS_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/main.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world";
+    return 0;
+}
