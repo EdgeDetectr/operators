ef8c0f87408d3fdff9933cd23eaabeacb750a08e
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-12-30 15:07:02 -0500

    clean up main

--- main.cpp
+++ main.cpp
@@ -13,24 +13,20 @@ void applyOperator(GradientOperator* operatorPtr, const string& inputPath, const
 }
 
 int main() {
-    try {
-        string projectPath = PathHelper::getProjectPath();
-        string inputPath = projectPath + "/test/gradient/datasets/photo.jpg";
-        string outputPath = "output.png";
+    string projectPath = PathHelper::getProjectPath();
+    string inputPath = projectPath + "/test/gradient/datasets/photo.jpg";
+    string outputPath = "output.png";
 
 //        opencv sobel
-        applyOperator(new OcvSobel(), inputPath, outputPath);
+    applyOperator(new OcvSobel(), inputPath, outputPath);
+
 //        alt sobel
-        applyOperator(new AltSobel(), inputPath, outputPath);
+    applyOperator(new AltSobel(), inputPath, outputPath);
 
 //        openmp sobel - 50% of alt sobel
-        omp_set_num_threads(1);
-        applyOperator(new OmpSobel(), inputPath, outputPath);
+    omp_set_num_threads(1);
+    applyOperator(new OmpSobel(), inputPath, outputPath);
 
-    } catch (const exception& ex) {
-        cerr << "Error: " << ex.what() << endl;
-        return 1;
-    }
 
     return 0;
 }
